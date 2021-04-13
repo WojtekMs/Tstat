2b9c65b8d71bd0339a4458b0cffa75d2e3fa4f37
WojtekMs <wojtek.atmo@gmail.com>
2021-04-14 00:50:34 +0200

    Project restructuring & networking submodule added
    Added networking git submodule into the project
    Removed all networking code from tstat repository
    
    Added install target
    (available 2 components):
    - networking
    - tstat

--- src/networking/networking/config.hpp
+++
@@ -1,9 +0,0 @@
-namespace networking::config
-{
-constexpr char SERVER_IP_ADDR[] = "127.0.0.1";
-constexpr int SERVER_PORT = 5003;
-// in bytes / chars
-// the first 5 bytes + '\0' contain the size of the message that is being transfered
-constexpr int MESSAGE_HEADER_SIZE = 6;
-
-}  // namespace networking::config
